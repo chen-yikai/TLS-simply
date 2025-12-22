535bb00cb4120e6dbd7c368f1f147eaa5f1dd991
elias <eliaschen.dev@gmail.com>
2025-12-22 23:11:06 +0800

    fix: remvoe proxy

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,29 +13,31 @@ export default function Footer() {
     "https://twtsl.ccu.edu.tw",
   ];
   return (
-    <footer className="flex flex-col items-center justify-center border rounded-lg mt-10">
-      <Accordion type="single" className="w-full" collapsible>
-        <AccordionItem value="data-source" className="px-5">
-          <AccordionTrigger>資料來源</AccordionTrigger>
-          <AccordionContent className="px-2">
-            <ul>
-              {dataSource.map((line, index) => (
-                <li key={index} className="text-gray-500">
-                  {index + 1}. {line}
-                </li>
-              ))}
-            </ul>
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="copyright" className="px-5">
-          <AccordionTrigger>版權聲明</AccordionTrigger>
-          <AccordionContent className="px-2">
-            <p className="text-gray-500">
-              手語通 {new Date().getFullYear()} EliasChen, MIT License
-            </p>
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+    <footer className="p-2">
+      <div className="flex flex-col items-center justify-center border rounded-lg mt-10">
+        <Accordion type="single" className="w-full" collapsible>
+          <AccordionItem value="data-source" className="px-5">
+            <AccordionTrigger>資料來源</AccordionTrigger>
+            <AccordionContent className="px-2">
+              <ul>
+                {dataSource.map((line, index) => (
+                  <li key={index} className="text-gray-500">
+                    {index + 1}. {line}
+                  </li>
+                ))}
+              </ul>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="copyright" className="px-5">
+            <AccordionTrigger>版權聲明</AccordionTrigger>
+            <AccordionContent className="px-2">
+              <p className="text-gray-500">
+                手語通 {new Date().getFullYear()} EliasChen, MIT License
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
     </footer>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,17 +3,19 @@ import { Input } from "./ui/input";
 
 export default function Header() {
   return (
-    <header className="flex-row flex items-center justify-between px-5 py-3 top-0 border rounded-b-lg fixed max-w-4xl w-full bg-white">
-      <div>
-        <Link href="/">
-          <h1 className="text-md font-bold hover:underline underline-offset-4">
-            手語通
-          </h1>
-        </Link>
+    <header className="top-0 fixed max-w-4xl w-full px-2">
+      <div className="border rounded-b-lg bg-white w-full px-5 py-3 flex-row flex items-center justify-between ">
+        <div>
+          <Link href="/">
+            <h1 className="text-md font-bold hover:underline underline-offset-4">
+              手語通
+            </h1>
+          </Link>
+        </div>
+        <nav>
+          <Input type="search" placeholder="Search..." className="w-64" />
+        </nav>
       </div>
-      <nav>
-        <Input type="search" placeholder="Search..." className="w-64" />
-      </nav>
     </header>
   );
 }
