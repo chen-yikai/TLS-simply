476f06c8ad5afa19cd96f4c9971c2d3569827a2c
elias <eliaschen.dev@gmail.com>
2025-12-25 10:03:19 +0800

    refector: move translate into page.tsx

--- app/translate/layout.tsx
+++ app/translate/layout.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "翻譯 | 手語通",
+  description: "將語句使用存在於辭典中的條目表達",
+};
 
 export default function TranslateLayout({ children }: { children: ReactNode }) {
   return <>{children}</>;
